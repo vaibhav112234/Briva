35b6ea21a1bb1816a5b2276a02842cef50893c41
CHINMAYpatkar <chinmaypatkar2999@gmail.com>
2025-04-30 14:52:05 +0530

    Heading

--- components/Products/Card.js
+++ components/Products/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HiStar } from 'react-icons/hi';
+import { FaStar } from 'react-icons/fa';
 const Card = () => {
   return (
     <div className="relative mt-10 mb-10 flex flex-col w-full max-w-xs overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md h-full">
@@ -28,36 +28,17 @@ const Card = () => {
             <span className="text-sm text-slate-900 line-through">$699</span>
           </p>
           <div className="flex items-center">
-            <HiStar className="h-5 w-5 text-yellow-500" />
-            <HiStar className="h-5 w-5 text-yellow-500" />
-            <HiStar className="h-5 w-5 text-yellow-500" />
-            <HiStar className="h-5 w-5 text-yellow-500" />
-            <HiStar className="h-5 w-5 text-yellow-500" />
-            <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
-              5.0
+            <FaStar color="gold"/>
+            <FaStar color="gold"/>
+            <FaStar color="gold"/>
+            <FaStar color="gold"/>
+            <FaStar color="gold"/>
+            <span className="mr-2 ml-3 rounded bg-yellow-500 px-2.5 py-0.5 text-xs font-semibold">
+             5.0
             </span>
           </div>
         </div>
-        <a
-          href="#"
-          className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="mr-2 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          Add to cart
-        </a>
+       
       </div>
     </div>
   );
--- components/Products/Product.js
+++ components/Products/Product.js
@@ -3,12 +3,20 @@ import Card from "./Card";
 
 const Product = () => {
   return (
-    <div className=" h-auto bg-white flex flex-row gap-8">
+    <div className="h-auto bg-white px-6 py-8 shadow rounded-md">
+    <h1 className="text-3xl font-extrabold text-gray-800 mb-6 text-start uppercase tracking-wide">
+      Vijay Foot Wear
+    </h1>
+    
+    <div className="flex flex-wrap justify-start gap-6">
       <Card />
       <Card />
       <Card />
       <Card />
     </div>
+  </div>
+  
+
   );
 };
 
