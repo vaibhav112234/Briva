edac3c597366e1ab14a704c659679c58ec262ede
Akhil <acharekarakhil@gmail.com>
2025-04-30 11:56:44 +0530

    fix className

--- components/Products/Card.js
+++ components/Products/Card.js
@@ -2,35 +2,35 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div class="relative mt-10 mb-10 flex flex-col w-full max-w-xs overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md h-full">
+    <div className="relative mt-10 mb-10 flex flex-col w-full max-w-xs overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md h-full">
       <a
-        class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+        className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
         href="#"
       >
         <img
-          class="object-cover"
+          className="object-cover"
           src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
           alt="product image"
         />
-        <span class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+        <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
           39% OFF
         </span>
       </a>
-      <div class="mt-4 px-5 pb-5">
+      <div className="mt-4 px-5 pb-5">
         <a href="#">
-          <h5 class="text-xl tracking-tight text-slate-900">
+          <h5 className="text-xl tracking-tight text-slate-900">
             Nike Air MX Super 2500 - Red
           </h5>
         </a>
-        <div class="mt-2 mb-5 flex items-center justify-between">
+        <div className="mt-2 mb-5 flex items-center justify-between">
           <p>
-            <span class="text-3xl font-bold text-slate-900">$449</span>
-            <span class="text-sm text-slate-900 line-through">$699</span>
+            <span className="text-3xl font-bold text-slate-900">$449</span>
+            <span className="text-sm text-slate-900 line-through">$699</span>
           </p>
-          <div class="flex items-center">
+          <div className="flex items-center">
             <svg
               aria-hidden="true"
-              class="h-5 w-5 text-yellow-300"
+              className="h-5 w-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +39,7 @@ const Card = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="h-5 w-5 text-yellow-300"
+              className="h-5 w-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -48,7 +48,7 @@ const Card = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="h-5 w-5 text-yellow-300"
+              className="h-5 w-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +57,7 @@ const Card = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="h-5 w-5 text-yellow-300"
+              className="h-5 w-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -66,25 +66,25 @@ const Card = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="h-5 w-5 text-yellow-300"
+              className="h-5 w-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
-            <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
+            <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
               5.0
             </span>
           </div>
         </div>
         <a
           href="#"
-          class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="mr-2 h-6 w-6"
+            className="mr-2 h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
